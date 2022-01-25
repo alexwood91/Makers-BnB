7dc57f9da59094156f12956cc8cc2e4b9eba79d1
Jackie Benn <jackiebenn84@hotmail.com>
2022-01-25 11:35:47 +0000

    rebase onto main

--- spec/features/viewing_rooms_spec.rb
+++ spec/features/viewing_rooms_spec.rb
@@ -7,6 +7,8 @@ feature 'viewing rooms' do
   end
 
   scenario 'a user sees a list of rooms' do
+    connection = PG.connect(dbname: 'makersbnb_test')
+    connection.exec("INSERT INTO rooms VALUES (1, 'Premier Inn');")
     visit ('/available')
     expect(page).to have_content "Premier Inn"
   end
---
+++ spec/setup_test_database.rb
@@ -0,0 +1,9 @@
+require 'pg'
+
+def setup_test_database
+  p "Setting up test database..."
+
+  connection = PG.connect(dbname: 'makersbnb_test')
+
+  connection.exec("TRUNCATE rooms;")
+end
