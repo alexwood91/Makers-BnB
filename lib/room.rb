2f25dd307e67e67883a80635ecdfd188d3f0e45e
Alex <alex.wood91@outlook.com>
2022-01-28 15:21:35 +0000

    updated with main

--- lib/room.rb
+++ lib/room.rb
@@ -50,16 +50,19 @@ class Room
   end
 
   def self.available(bookfrom, bookto)
-    result = Database.query("SELECT * FROM rooms WHERE datefrom <= $1 and dateto >= $2;", [bookfrom, bookto])   
+    result = Database.query("SELECT * FROM rooms WHERE datefrom <= $1 and dateto >= $2;", 
+[bookfrom, bookto])   
     result.map do |room|
-    Room.new(roomid: room['roomid'], name: room['name'], description: room['description'], price: room['price'], datefrom: room['datefrom'], dateto: room['dateto'], userid: room['userid'])
+      Room.new(roomid: room['roomid'], name: room['name'], description: room['description'], 
+price: room['price'], datefrom: room['datefrom'], dateto: room['dateto'], userid: room['userid'])
     end
   end
 
   def self.find(roomid)
     result = Database.query("SELECT * FROM rooms WHERE roomid = $1;", [roomid])
     rooms = result.map do |room|
-      Room.new(roomid: room['roomid'], name: room['name'], description: room['description'], price: room['price'], datefrom: room['datefrom'], dateto: room['dateto'], userid: room['userid'])
+      Room.new(roomid: room['roomid'], name: room['name'], description: room['description'], 
+price: room['price'], datefrom: room['datefrom'], dateto: room['dateto'], userid: room['userid'])
     end
     rooms.first
   end
